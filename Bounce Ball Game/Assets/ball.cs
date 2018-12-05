ccafbc36b16e8ff26df3439e7d1d4e1b5e51bf12
Bryan <45637546+Dragola@users.noreply.github.com>
2018-12-05 16:21:37 -0500

    The whole unity project
    So far gave ball gravity and collision, have to detect collision and make it bounce in opposite direction (unless unity has this built in).

---
+++ Bounce Ball Game/Assets/ball.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour {
+    GameObject ball_object;
+
+	// Use this for initialization (input, etc.)
+	void Start () {
+        ball_object = GameObject.Find("Ball"); //gets ball gameobject
+	}
+	
+	// Update is called once per frame (use for physics)
+	void Update () {
+		
+	}
+}
