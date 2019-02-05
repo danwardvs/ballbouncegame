e5aeca550d0a9e645cf50b0252e31545653bdf27
danwardvs <danwardvs@adsgames.net>
2019-02-05 12:11:43 -0500

    Slight reorganization of EndPoint block and added loading of next level, example level 2 created.

---
+++ BallBounceGame/Assets/EndBlock/WinTextScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinTextScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    void OnMouseDown()
+    {
+        if (SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            SceneManager.LoadScene(1, LoadSceneMode.Single);
+        }
+        if (SceneManager.GetActiveScene().buildIndex == 1)
+        {
+            SceneManager.LoadScene(2, LoadSceneMode.Single);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
