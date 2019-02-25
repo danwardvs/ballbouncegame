87c1238548c33c45e2b1a701ff59eac636aa4919
Bryan Gadd <45637546+Dragola@users.noreply.github.com>
2019-02-25 15:45:28 -0500

    Added sliding machanic to end point
    Currently uses 2 points but plan to have multiple (move around level)

--- BallBounceGame/Assets/EndBlock/EndPoint.cs
+++ BallBounceGame/Assets/EndBlock/EndPoint.cs
@@ -5,23 +5,46 @@ using UnityEngine;
 public class EndPoint : MonoBehaviour
 {
     public GameObject winText;
+    bool endMove = false;
+    int moveDirection = 3;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //if sliding is in level
+        if(GameObject.FindGameObjectsWithTag("sliders").Length > 0)
+        {
+            endMove = true;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (endMove == true)
+        {
+            gameObject.transform.Translate(moveDirection * Time.deltaTime, 0, 0);
+        }
+    }
+
+    void FixedUpdate ()
+    {
         
     }
+
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "ball")
         {
             winText.SetActive(true);
+        }
+        if(other.gameObject.name == "pointL")
+        {
+            moveDirection = 3;
+        }
+        if (other.gameObject.name == "pointR")
+        {
+            moveDirection = -3;
         }
     }
 }
