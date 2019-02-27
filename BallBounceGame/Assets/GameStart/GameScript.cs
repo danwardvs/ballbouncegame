7bd6c1fc7ede928174a722b8ac54a8adeb59d4ce
Danny Van Stemp <danwardvs@adsgames.net>
2019-02-27 14:57:47 -0500

    Power limited, displays stats of shot. Also added GameGUI that should be included with every level

--- BallBounceGame/Assets/GameStart/GameScript.cs
+++ BallBounceGame/Assets/GameStart/GameScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameScript : MonoBehaviour
 {
@@ -11,6 +12,9 @@ public class GameScript : MonoBehaviour
 
     // We store a reference to this so we can change colour without re-getting the object
     SpriteRenderer m_SpriteRenderer;
+
+    Text gameText;
+    Text angleText;
 
     // Variables for the click and drag
     bool is_clicked = false;
@@ -30,6 +34,9 @@ public class GameScript : MonoBehaviour
         // Snag a reference to the arrow for later use
         arrowObject = GameObject.Find("arrow");
 
+        // The text object that should be included with every level
+        gameText = GameObject.Find("ShotText").GetComponent<Text>();
+
 
 
     }
@@ -39,7 +46,7 @@ public class GameScript : MonoBehaviour
     {
 
         Vector2 mouse_location = Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
-
+        
 
         if (Input.GetMouseButtonDown(0))
         {
@@ -75,15 +82,23 @@ public class GameScript : MonoBehaviour
             new_force = new Vector2(transform.position.x, transform.position.y) - mouse_location;
 
             // We use MATH and FUNCTIONS to find angles
-            float newAngle = 90 + -57 * Mathf.Atan2(new_force.x, new_force.y);
-            
-            // Set the rotation of the arrow to show current trajectory of ball
-            arrowObject.transform.eulerAngles = new Vector3(0, 0, newAngle);
+            float new_angle = 90 + -57 * Mathf.Atan2(new_force.x, new_force.y);
 
+            // Set the rotation of the arrow to show current trajectory of ball
+            arrowObject.transform.eulerAngles = new Vector3(0, 0, new_angle);
+
+            // Limit power to abritary value given here
+            float max_force = 7;
+
+            if (new_force.magnitude > max_force)
+                new_force = new_force * (max_force / new_force.magnitude);
 
             // Set visual cues
             m_SpriteRenderer.color = Color.red;
             arrowObject.SetActive(true);
+            gameText.enabled = true;
+
+            gameText.text = "Power:" + new_force.magnitude.ToString("#.#") + "\nAngle:" + new_angle.ToString("#");
 
 
         }
@@ -92,6 +107,7 @@ public class GameScript : MonoBehaviour
             // Reset visual cues
             m_SpriteRenderer.color = Color.green;
             arrowObject.SetActive(false);
+            gameText.enabled = false;
 
         }
     }
