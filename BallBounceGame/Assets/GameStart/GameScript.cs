a58ccd86aed3674e189b6fa7b1cbb7101699e62a
danwardvs <danwardvs@adsgames.net>
2019-02-27 10:33:56 -0500

    Added arrow to game start, cleaned up game start script (what a mess!)

--- BallBounceGame/Assets/GameStart/GameScript.cs
+++ BallBounceGame/Assets/GameStart/GameScript.cs
@@ -4,52 +4,64 @@ using UnityEngine;
 
 public class GameScript : MonoBehaviour
 {
+    // References to gameObjects that are used by this gameObject
     public GameObject GameBallPrefab;
     GameObject GameBallInstance;
-    bool is_clicked = false;
+    GameObject arrowObject;
 
+    // We store a reference to this so we can change colour without re-getting the object
     SpriteRenderer m_SpriteRenderer;
+
+    // Variables for the click and drag
+    bool is_clicked = false;
+    Vector2 new_force;
+    Vector2 mouse_location;
+    
+
+    
     // Start is called before the first frame update
     void Start()
     {
+        //Grab 
         m_SpriteRenderer = GetComponent<SpriteRenderer>();
         //Set the GameObject's Color quickly to a set Color (blue)
         m_SpriteRenderer.color = Color.green;
+
+        arrowObject = GameObject.Find("arrow");
+
+
 
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        Vector2 mouse_location = Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
+
+
         if (Input.GetMouseButtonDown(0))
         {
 
-            Vector2 mouseLocation = Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
-
+            // Hitbox for location of game start object, (currently is triangle)
             float width = 2;
             float height = 2;
 
-            if (mouseLocation.x > transform.position.x - width / 2 && mouseLocation.x < transform.position.x + width / 2
-                && mouseLocation.y > transform.position.y - height / 2 && mouseLocation.y < transform.position.y + height / 2)
-            {
-            
-                is_clicked = true;
-            }
-            else
-            {
-                is_clicked = false;
-            }
 
+            // Simple AABB collision for clicking on the game start object
+            is_clicked = (mouse_location.x > transform.position.x - width / 2 && mouse_location.x < transform.position.x + width / 2
+                && mouse_location.y > transform.position.y - height / 2 && mouse_location.y < transform.position.y + height / 2);
+           
         }
+        // Handles left click being released
         if (Input.GetMouseButtonUp(0))
         {
             if (is_clicked)
             {
-                Vector2 target = Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
-                Vector2 newForce = new Vector2(transform.position.x, transform.position.y) - target;
+                // If game start object was being clicked and dragged, spawn a game ball
 
                 GameBallInstance = Instantiate(GameBallPrefab, transform);
-                GameBallInstance.GetComponent<Rigidbody2D>().AddForce(newForce * 100);
+                GameBallInstance.GetComponent<Rigidbody2D>().AddForce(new_force * 100);
             }
             is_clicked = false;
     
@@ -58,12 +70,27 @@ public class GameScript : MonoBehaviour
         }
         if (is_clicked)
         {
+            // Calculate a force based on mouse location and game start object position
+            new_force = new Vector2(transform.position.x, transform.position.y) - mouse_location;
+
+            // We use MATH and FUNCTIONS to find angles
+            float newAngle = 90 + -57 * Mathf.Atan2(new_force.x, new_force.y);
+            
+            // Set the rotation of the arrow to show current trajectory of ball
+            arrowObject.transform.eulerAngles = new Vector3(0, 0, newAngle);
+
+
+            // Set visual cues
             m_SpriteRenderer.color = Color.red;
+            arrowObject.active = true;
+
 
         }
         else
         {
+            // Reset visual cues
             m_SpriteRenderer.color = Color.green;
+            arrowObject.active = false;
 
         }
     }
