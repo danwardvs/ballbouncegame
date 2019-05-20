4f6239f588a6b53f6b11a063c984e3a825ad2c08
danwardvs <danwardvs@adsgames.net>
2019-05-20 17:22:51 -0400

    some portal work done

--- BallBounceGame/Assets/Portal/PortalScript.cs
+++ BallBounceGame/Assets/Portal/PortalScript.cs
@@ -4,21 +4,32 @@ using UnityEngine;
 
 public class PortalScript : MonoBehaviour
 {
+    GameObject sibling_portal_collider;
     GameObject sibling_portal;
 
+
+
     // Start is called before the first frame update
+
+
     void Start()
     {
         // Find the sibling portal
         if (gameObject.name == "Portal_1")
         {
             sibling_portal = gameObject.transform.parent.Find("Portal_2").gameObject;
+            sibling_portal_collider = gameObject.transform.parent.Find("Portal_2/Collider").gameObject;
+
+
 
         }
         else
         {
             sibling_portal = gameObject.transform.parent.Find("Portal_1").gameObject;
+            sibling_portal_collider = gameObject.transform.parent.Find("Portal_1/Collider").gameObject;
+
         }
+
     }
 
     // Update is called once per frame
@@ -31,7 +42,21 @@ public class PortalScript : MonoBehaviour
     {
         if (other.gameObject.tag == "ball")
         {
-            other.gameObject.transform.position = sibling_portal.transform.position;
+            Rigidbody2D rb = other.gameObject.GetComponent<Rigidbody2D>();
+            other.gameObject.transform.position = sibling_portal_collider.transform.position;
+            //rb.velocity = transform.up * rb.velocity.magnitude;
+
+            float z = sibling_portal.transform.rotation.eulerAngles.z;
+            float m = rb.velocity.magnitude;
+            print(z);
+
+            Vector3 v = new Vector3(Mathf.Sin(z*(Mathf.PI)/180) * m, Mathf.Cos(z*(Mathf.PI) / 180) * m, 0);
+
+            print(v);
+
+
+
+            rb.velocity = v;
 
         }
     }
