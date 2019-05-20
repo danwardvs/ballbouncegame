d0ce04f816086d44eac304723dabc56c2113e84e
danwardvs <danwardvs@adsgames.net>
2019-05-20 01:23:16 -0400

    Started a haskish portal

---
+++ BallBounceGame/Assets/Portal/PortalScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalScript : MonoBehaviour
+{
+    GameObject sibling_portal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Find the sibling portal
+        if (gameObject.name == "Portal_1")
+        {
+            sibling_portal = gameObject.transform.parent.Find("Portal_2").gameObject;
+
+        }
+        else
+        {
+            sibling_portal = gameObject.transform.parent.Find("Portal_1").gameObject;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "ball")
+        {
+            other.gameObject.transform.position = sibling_portal.transform.position;
+
+        }
+    }
+}
