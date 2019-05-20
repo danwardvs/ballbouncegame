a3678ae74ef26245255a31f6dad675a34a41e081
alegemaate <alegemaate@gmail.com>
2019-05-20 16:32:09 -0400

    Added level select ui (autopopulates)

--- BallBounceGame/Assets/LevelSelectScript.cs
+++ BallBounceGame/Assets/LevelSelectScript.cs
@@ -6,33 +6,61 @@ using UnityEngine.UI;
 
 public class LevelSelectScript : MonoBehaviour
 {
-    public GameObject buttonPrefab;
+    public GameObject ButtonPrefab;
+    int SceneStart = 2;
 
     // Start is called before the first frame update
     void Start()
     {
         // Create buttons
-        PopulateButtons(CreatePanel(), 5);
+        PopulateButtons(CreatePanel(), SceneManager.sceneCountInBuildSettings - SceneStart);
     }
 
     // Populate buttons
     void PopulateButtons(GameObject panel, int num)
     {
+        int row = 0;
+        int column = 0;
+
         for (int i = 0; i < num; i++)
-        {
-            // Create new button and assign as child
-            GameObject levelButton = Instantiate(buttonPrefab, new Vector3(0, 100.0f + i * -100.0f, 0), Quaternion.identity);
-            levelButton.transform.SetParent(panel.transform, false);
-            levelButton.transform.localScale = new Vector3(1, 1, 1);
+        {   
+            // Level
+            int level = i + 1;
 
-            // Set on click
-            Button b = levelButton.GetComponent<Button>();
+            // Create button
+            CreateButton(-270.0f + column * 60.0f, -50.0f - row * 60.0f, level, panel);
 
-            if (b != null)
+            // Next row
+            column++;
+            if (column >= 10)
             {
-                int t = i + 1;
-                b.onClick.AddListener(() => LoadLevel(t));
+                row++;
+                column = 0;
             }
+        }
+    }
+
+    // Create button
+    void CreateButton(float x, float y, int level, GameObject parent)
+    {
+        // Create new button and assign as child
+        GameObject levelButton = Instantiate(ButtonPrefab, new Vector3(x, y, 0), Quaternion.identity);
+        levelButton.transform.SetParent(parent.transform, false);
+        levelButton.transform.localScale = new Vector3(1, 1, 1);
+
+        RectTransform buttonTrans = levelButton.GetComponent<RectTransform>();
+        buttonTrans.anchorMin = new Vector2(0.5f, 1.0f);
+        buttonTrans.anchorMax = new Vector2(0.5f, 1.0f);
+
+        // Text
+        levelButton.GetComponentInChildren<Text>().text = (level).ToString();
+
+        // Set on click
+        Button b = levelButton.GetComponent<Button>();
+
+        if (b != null)
+        {
+            b.onClick.AddListener(() => LoadLevel(level));
         }
     }
 
@@ -47,11 +75,8 @@ public class LevelSelectScript : MonoBehaviour
         RectTransform panelTrans = panel.GetComponent<RectTransform>();
         panelTrans.anchorMin = new Vector2(0.0f, 0.0f);
         panelTrans.anchorMax = new Vector2(1.0f, 1.0f);
-        panelTrans.pivot = new Vector2(0.5f, 0.5f);
-        panelTrans.offsetMin = new Vector2(124, panelTrans.offsetMin.y);
-        panelTrans.offsetMax = new Vector2(-124, panelTrans.offsetMax.y);
-        panelTrans.offsetMax = new Vector2(panelTrans.offsetMax.x, -100.0f);
-        panelTrans.offsetMin = new Vector2(panelTrans.offsetMin.x, 50.0f);
+        panelTrans.offsetMin = new Vector2(90, 50.0f);
+        panelTrans.offsetMax = new Vector2(-90, -130.0f);
 
         Image panelImage = panel.AddComponent<Image>();
         panelImage.color = Color.black;
@@ -63,7 +88,7 @@ public class LevelSelectScript : MonoBehaviour
     public void LoadLevel(int newLevel)
     {
         // Since the level select and title screen come before the game levels in the build level list, we add one to it
-        newLevel += 1;
+        newLevel += SceneStart - 1;
         SceneManager.LoadScene(newLevel, LoadSceneMode.Single);
     }
 
