16cc4afc06409c995ee70ccafcd0da6cd136e13c
danwardvs <danwardvs@adsgames.net>
2019-06-09 12:21:13 -0400

    Portals updated, mostly visual improvements

---
+++ BallBounceGame/Assets/Portal/PortalParentScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalParentScript : MonoBehaviour
+{
+
+    public Color portal_colour;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
