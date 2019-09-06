fa2a5a5b13aa652f893a2c0eeb3278320d45e075
danwardvs <danwardvs@adsgames.net>
2019-09-06 17:23:50 -0400

    Super bounce on rotational block in lvl 10 and FPS counter

---
+++ BallBounceGame/Assets/GameGUI/FPSCounterScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+
+public class FPSCounterScript : MonoBehaviour
+{
+
+    float updateInterval = 0.5f;
+
+    private float accum = 0.0f; // FPS accumulated over the interval
+    private int frames = 0; // Frames drawn over the interval
+    private float timeleft; // Left time for current interval
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeleft = updateInterval;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeleft -= Time.deltaTime;
+        accum += Time.timeScale / Time.deltaTime;
+        ++frames;
+
+        // Interval ended - update GUI text and start new interval
+        if (timeleft <= 0.0)
+        {
+            // display two fractional digits (f2 format)
+            gameObject.GetComponent<Text>().text = "" + (accum / frames).ToString("f2");
+            timeleft = updateInterval;
+            accum = 0.0f;
+            frames = 0;
+        }
+    }
+}
