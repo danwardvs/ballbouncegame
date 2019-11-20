c4e022ce50d8e492259239889b1fe2397944926a
danwardvs <danwardvs@adsgames.net>
2019-11-20 16:43:57 -0500

    Overhauled sprites and lighting

---
+++ BallBounceGame/Assets/LevelBlock/GameBlockScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameBlockScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpriteRenderer block_sprite_renderer = GetComponent<SpriteRenderer>();
+        UnityEngine.Experimental.Rendering.Universal.Light2D area_light = GameObject.Find("BlockLight").GetComponent<UnityEngine.Experimental.Rendering.Universal.Light2D>();
+        
+        area_light.color = block_sprite_renderer.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
