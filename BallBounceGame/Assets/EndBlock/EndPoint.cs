c002eccb34d15c4727b4ac38c939c288e9cb86b6
danwardvs <danwardvs@adsgames.net>
2019-11-21 17:58:16 -0500

    Lighting tweaks and breathing lights on level finish

--- BallBounceGame/Assets/EndBlock/EndPoint.cs
+++ BallBounceGame/Assets/EndBlock/EndPoint.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,17 +7,24 @@ public class EndPoint : MonoBehaviour
 {
     GameObject GameGUI;
     GameObject GameStart;
+    UnityEngine.Experimental.Rendering.Universal.Light2D LightPoint;
+    private float clock = 0;
     // Start is called before the first frame update
     void Start()
     {
         GameGUI = GameObject.Find("GameGUI");
         GameStart = GameObject.Find("GameStart");
+        LightPoint = this.transform.Find("Light_Point").GetComponent<UnityEngine.Experimental.Rendering.Universal.Light2D>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        clock += Time.deltaTime/1f;
+        LightPoint.intensity = (float)( 4 + 2.5f*Math.Sin(clock));
         
+
     }
 
     void FixedUpdate ()
