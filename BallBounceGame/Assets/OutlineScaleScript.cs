4a0f002d59f11ff748f7be62617676519e6a48e8
Danny Van Stemp <danwardvs@adsgames.net>
2020-01-22 09:16:09 -0500

    Started outline of static blocks properly

---
+++ BallBounceGame/Assets/OutlineScaleScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutlineScaleScript : MonoBehaviour
+{
+    GameObject insideBlock;
+
+    // Start is called before the first frame update
+    [ExecuteInEditMode]
+    void Awake()
+    {
+        float x = transform.localScale.x;
+        float y = transform.localScale.y;
+        insideBlock = transform.Find("InsideBlock").gameObject;
+        insideBlock.transform.localScale = new Vector3(0.9f + (0.1f -0.1f/x) ,0.9f + (0.1f -0.1f/y),1);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
