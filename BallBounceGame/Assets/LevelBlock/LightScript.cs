4756c6ac0bbb99652b0addc4b764da5d5d1a5fb2
Danny Van Stemp <danwardvs@adsgames.net>
2020-08-10 23:40:23 -0400

    Updated to all new levels

--- BallBounceGame/Assets/LevelBlock/LightScript.cs
+++ BallBounceGame/Assets/LevelBlock/LightScript.cs
@@ -9,7 +9,6 @@ public class LightScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        print(QualitySettings.GetQualityLevel());
         if(QualitySettings.GetQualityLevel()<importanceLevel)
 
             gameObject.SetActive(false);
